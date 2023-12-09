efa4bff93b7d659f7e26f78410c3c238b8e891a2
BilyalovE <eldar2117@gmail.com>
2023-12-09 15:59:03 +0300

    Input-output function

--- Parties/Parties.cpp
+++ Parties/Parties.cpp
@@ -41,32 +41,39 @@ Input_solver_parametres input_function(Pipeline_parameters input)
                                    // начальное значение измеряемого парметра, 
                                    // входной массив измеряемых парметров )
 
-double* solver(Input_solver_parametres value, vector <double>* initial_buffer, vector <double>* buffer)
+vector <double> solver(Input_solver_parametres value, vector <double>* initial_buffer, double* ptr)
 {
     vector <double> layer_0(value.n);  // Начальный слой
     layer_0 = *initial_buffer;
 
     vector <double> layer_1(value.n);  // Следующий слой 
-    layer_1[0] = (*buffer)[value.number_layers + 1];
+    layer_1[0] = *ptr;
     for (int i{ 1 }; i < value.n; i++)
     {
         layer_1[i] = layer_0[i - 1];
-        cout << layer_1[i] << endl;
+        //cout << layer_1[i] << endl;
     }
     // По методу характеристик текущий слой(рассчитанный на данной итерации) 
     // становится начальным слоем на следующей итерации для рассчёта следующего слоя
     layer_0 = layer_1;
-    //return layer_1;
+    return layer_1;
 
 }
 
-
+void output_function(int j, Input_solver_parametres value, vector <double>* sloi) {
+    vector <double> output_sloi(value.n);
+    output_sloi = *sloi;
+    cout << "Слой" << j + 1 << endl;
+    for (int i{ 0 }; i < value.n; i++) {
+        cout << output_sloi[i] << endl;
+    }
+}
 
 int main()
 {
     // Объявление структуры с именем Pipeline_parameters для переменной method_characteristics
     Pipeline_parameters pipeline_characteristics;
-    
+
     setlocale(LC_ALL, "Russian"); // Корректный вывод кириллицы
 
     Input_solver_parametres input_parametres = input_function(pipeline_characteristics);
@@ -76,9 +83,9 @@ int main()
     vector <double> ro_0(input_parametres.n);  // Начальный слой по плотности
 
     vector <double> ro_in(input_parametres.number_layers); // Вектор плотностей нефти входных партий 
-    ro_in = { 870, 870, 870, 870, 880, 880, 880, 880, 0, 0, 0, 0, 0 };
+    ro_in = { 890, 870, 870, 870, 880, 880, 880, 880, 0, 0, 0, 0, 0 };
 
-
+    vector <double> sloi(input_parametres.n);
     // Предполагаем, что в начальный момент времени всю трубу заполняют нефть с начальными параметрами initial_value
     cout << "Заполнение трубы нефтью в начале моделирования" << endl;
     for (int i{ 0 }; i < input_parametres.n; i++)
@@ -86,11 +93,18 @@ int main()
         ro_0[i] = ro_n;
         cout << ro_0[i] << endl;
     }
-
+    //input_parametres.number_layers
     for (int j{ 0 }; j < input_parametres.number_layers; j++)
     {
-        cout << "Слой" << j + 1 << endl;
-        solver(input_parametres, &ro_0, &ro_in);
+        sloi = solver(input_parametres, &ro_0, &ro_in[j]);
+        output_function(j, input_parametres, &sloi);
+        ro_0 = sloi;
     }
+
+    cout << "все плохо";
+    system("pause");
+
+    return 0;
+
 
 }
